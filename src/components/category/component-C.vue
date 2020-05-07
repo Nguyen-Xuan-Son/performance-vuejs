496c4daa509a191f2f7e08f129037ec64057d0e9
Son NX <nguyenxuanson22299@gmail.com>
2020-05-07 22:48:06 +0700

    Update

--- src/components/category/component-C.vue
+++ src/components/category/component-C.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="row">
-        ComponentA
+        ComponentC
     </div>
 </template>
 
 <script>
 
 export default {
-    name: 'ComponentA',
+    name: 'ComponentC',
 }
 </script>
 
